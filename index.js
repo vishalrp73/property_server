a898ae8901b3edcdc685da364c696b7ba8a3985b
Vishal Prakash <v.r.prakash73@gmail.com>
2021-12-04 14:49:01 +1300

    first commit of basic endpoints using json file for database

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const port = 4000;
+const db = require('./properties.json');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send('Connection to server successful !');
+});
+
+app.get('/properties', (req, res) => {
+    res.status(200).send(db);
+});
+
+app.post('/getProperty', (req, res) => {
+    propertyId = req.body.propertyId
+    db.forEach(property => {
+        if (property.id == propertyId) {
+            res.status(200).send(property);
+        }
+    })
+})
+
+app.listen(port, () => console.log('Express server running on port', port));
